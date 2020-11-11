005654d5e01793c2b8ac87bc4146fc178b96b78f
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-11-10 20:24:12 -0600

    10: Webpack - Empaquetando Nuestros Modulos

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const HelloWorld = () => {
+    return(
+        <div>
+            <h1>HelloWorld</h1>
+        </div>
+    )
+}
+
+export default HelloWorld;

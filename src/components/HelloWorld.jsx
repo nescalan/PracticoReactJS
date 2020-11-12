e515602ad9072acae0ba62d10c7bacd3d3cf7f35
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-11-11 20:25:49 -0600

    Revisar App.scss

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import '../assets/styles/App.scss';
-=======
-import '../assets/styles/estilos';
->>>>>>> 6372f28016c91a98920db6262cdd209b60587707
+// import '../assets/styles/App.scss';
 
 const HelloWorld = () => (
     <h1>HelloWorld</h1>

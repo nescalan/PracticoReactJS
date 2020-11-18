dfc8256979759777540ea6fce8df67175573117b
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-11-17 18:17:18 -0600

    22: Creando Fake API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,49 +1,62 @@
-import React from 'react';
-import Header from '../components/Header';
-import Search from '../components/Search';
-import '../assets/styles/App.scss';
-import Categories from '../components/Categories';
-import CarouselItem from '../components/CarouselItem';
-import Carousel from '../components/Carousel';
-import Footer from '../components/Footer';
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import CarouselItem from "../components/CarouselItem";
+import Carousel from "../components/Carousel";
+import Footer from "../components/Footer";
 
-const App = () => (
+import "../assets/styles/App.scss";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/initalState")
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
+  return (
     <div className="App">
-        <Header />
-        <Search />
+      <Header />
+      <Search />
 
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+      <Categories title="Mi Lista">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+      <Categories title="Tendencias">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Footer />
+      <Footer />
     </div>
-);
+  );
+};
 
 export default App;
